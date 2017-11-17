6e130560671b19673918e9d645fb248269aa2323
Wekoslav Stefanovski <swekster@gmail.com>
2017-11-17 22:24:34 +0100

    Final preparation

--- JSTalks/set-timeout.ts
+++ JSTalks/set-timeout.ts
@@ -1,17 +1,4 @@
-function delay(ms) {
-    return new Promise(resolve => setTimeout(()=>resolve(), ms));
-}
-
-async function prviDrugi(){
-    console.log("prvi");
-    await delay(1000);
-    console.log("drugi");
-    await delay(1000);
-    console.log("treci");
-    await delay(1000);
-    console.log("cetvrti");
-    await delay(1000);
-    console.log("peti");
-}
-
-prviDrugi();
+console.log("Един");
+console.log("Два");
+console.log("Три");
+console.log("Четири");
