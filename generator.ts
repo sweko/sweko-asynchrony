59a1630247bdec00b743299f042fc9ed3d12a4cc
Wekoslav Stefanovski <swekster@gmail.com>
2017-11-17 22:15:11 +0100

    added async generator

--- generator.ts
+++ generator.ts
@@ -13,23 +13,3 @@ for (var index = 0; index < 100; index++) {
     }
     console.log(value);
 }
-
-// async function* asyncRandomNumbers() {
-//     // This is a web service that returns a random number
-//     const url = 'https://www.random.org/decimal-fractions/?num=1&dec=10&col=1&format=plain&rnd=new';
-
-//     while (true) {
-//         const response = await fetch(url);
-//         const text = await response.text();
-//         yield Number(text);
-//     }
-// }
-
-// async function example() {
-//     for await (const number of asyncRandomNumbers()) {
-//         console.log(number);
-//         if (number > 0.95) break;
-//     }
-// }
-
-// example();
